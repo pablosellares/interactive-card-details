e9ae422613f37cd4c283fc358ec16f2fc30c2f15
Pablo Sellarés <pablosellares@live.com.ar>
2024-06-03 18:49:34 -0300

    Fix form validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -77,6 +77,23 @@ const Form = ({
     const number = e.target.value;
     setCardNumber(number);
     setErrors({ ...errors, cardNumber: validateCardNumber(number) });
+    const inputVal = e.target.value.replace(/ /g, ""); //remove all the empty spaces in the input
+    let inputNumbersOnly = inputVal.replace(/\D/g, ""); // Get only digits
+
+    if (inputNumbersOnly.length > 16) {
+      //If entered value has a length greater than 16 then take only the first 16 digits
+      inputNumbersOnly = inputNumbersOnly.substr(0, 16);
+    }
+
+    // Get nd array of 4 digits per an element EX: ["4242", "4242", ...]
+    const splits = inputNumbersOnly.match(/.{1,4}/g);
+
+    let spacedNumber = "";
+    if (splits) {
+      spacedNumber = splits.join(" "); // Join all the splits with an empty space
+    }
+
+    setCardNumber(spacedNumber); // Set the new CC number
   };
 
   const handleCardExpiresMonth = (e) => {
