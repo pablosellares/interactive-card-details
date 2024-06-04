153df114221dd08b3758e1c55108da66879f8001
Pablo Sellarés <pablosellares@MacBook-Pro-de-Pablo.local>
2024-06-04 11:28:49 -0300

    Add success component

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import iconCompleteImg from "../assets/icon-complete.svg";
 
 const Success = () => {
-  return <section className="success">Success</section>;
+  return (
+    <section className="success">
+      <img src={iconCompleteImg} className="success-img" alt="" />
+      <h1 className="success-title">Thank You!</h1>
+      <p className="success-paragraph">We've added your credit card details</p>
+      <form>
+        <button type="submit" className="success-btn">
+          Continue
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Success;
